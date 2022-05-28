3d6aaa381e48af2ae4a78d27010e57d2dea847fa
Ferass 'Vitali64' EL HAFIDI <vitali64pmemail@protonmail.com>
2022-05-28 12:16:22 +0200

    all: modify makefiles to be posix compliant and configurable (see config.mk)
    Signed-off-by: Ferass 'Vitali64' EL HAFIDI <vitali64pmemail@protonmail.com>

---
+++ src/config.mk
@@ -0,0 +1,19 @@
+# Utilities to be included in fasesiab
+SRC  =\
+		ls\
+		cat\
+		yes\
+		head
+
+# C compiler
+CC   = cc
+
+# C compiler options
+# - Use the C99 standard
+# - All warnings
+ARGS = -std=c99 -Wall
+
+# Path where utilities are installed
+PREFIX=/usr/local
+
+

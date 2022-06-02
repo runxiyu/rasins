1fc8e8fb1fdbf8811e0e6b5665d35435e447c0ab
Ferass 'Vitali64' EL HAFIDI <vitali64pmemail@protonmail.com>
2022-06-02 17:51:35 +0200

    cat: replace most functions with POSIX-compliant functions from 'unistd.h'
    Signed-off-by: Ferass 'Vitali64' EL HAFIDI <vitali64pmemail@protonmail.com>

--- src/cat/main.c
+++ src/cat/main.c
@@ -14,49 +14,52 @@
  *	You should have received a copy of the GNU General Public License
  *	along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#include <stdio.h>  /* To print the file's contents to stdout */
-#include <stdlib.h> /* For opening files */
+#include <fcntl.h>  /* For opening files */
 #include <string.h> /* To check for arguments */
+#include <unistd.h> /* POSIX-compliant library */
+
+/* Define a `print()` macro */
+#define print(string) write(STDOUT_FILENO, string, strlen(string)) 
 
 int main(int argc, char *argv[]) {
 	/* Check for arguments */
 	if(argc == 2) {
 		if (!strcmp(argv[1], "-h")) {
 			/* Print the help message */
-			printf("Ferass' Base System.\n");
-			printf("\n");
-			printf("Usage: %s [FILE]\n", argv[0]);
-			printf("\n");
-			printf("Concatenate FILE to stdout.\n");
-			printf("\n");
+			print("Ferass' Base System.\n\n"
+					"Usage: ");
+			print(argv[0]);
+			print(" [FILE]\n\n"
+					"Concatenate FILE to stdout\n\n");
 		}
 		else {
-			FILE *file; /* Define 'file' */
-			char s[512];
-			file=fopen(argv[1], "r"); /* Open the file in read-only mode */
-			if(file == NULL) /* Check if the file has been opened successfully */
+			int file; /* Define 'file' */
+			char s[200000]; /* Stupid character limit that will be removed soon */
+			file=open(argv[1], O_RDONLY); /* Open the file(argv[1]) in read-only(O_RDONLY) mode */
+			if(file == -1) /* Check if the file has been opened successfully */
 			{
-				printf("cat: %s: No such file or directory\n", argv[1]);
-				exit(1); /* If not, exit. */
+				print("cat: ");
+				print(argv[1]);
+				print(": No such file or directory\n");
+				return 1; /* If not, exit. */
 			}
-			while(fgets(s, 512, file) != NULL)
+			while(read(file, s, 200000) > 0)
 			{
-				s[strcspn(s, "\n")] = 0; /* Remove trailing newline */
 				/* Print the file's contents line by line to stdout */
-				puts(s); 
+				print(s);
 			}
-			fclose(file); /* Close the file */
+			close(file); /* Close the file */
 		}
 	}
 	else if(argc == 1)
 	{
 		/* Enter in a loop where each line typed gets printed to stdout */
 		while(1) {
-			/* Read user output */
-			char input[80];
-			fgets(input, 80, stdin);
+			/* Read user input */
+			char input[200000];
+			read(STDIN_FILENO, input, 200000);
 			/* Print it */
-			printf("%s", input);
+			print(input);
 		}
 	}
 	return 0;

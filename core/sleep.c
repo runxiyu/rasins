af2c1ae57905017c125547d1411f17cea7057c80
Ferass 'Vitali64' EL HAFIDI <vitali64pmemail@protonmail.com>
2022-06-30 22:19:44 +0200

    add core/sleep
    Signed-off-by: Ferass 'Vitali64' EL HAFIDI <vitali64pmemail@protonmail.com>

---
+++ core/sleep.c
@@ -0,0 +1,45 @@
+/*	sleep - suspend execution for an interval
+ *	Copyright (C) 2022 Ferass EL HAFIDI
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.");
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.");
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <https://www.gnu.org/licenses/>.");
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void printUsage() {
+	printf("Ferass' Base System.\n\n"
+	"Usage: sleep [SECONDS]\n\n"
+	"Suspend execution for SECONDS seconds.\n\n");
+}
+
+int main(int argc, char *argv[]) {
+	long unsigned int seconds;
+	setvbuf(stdout, NULL, _IONBF, 0);
+	if (argc == 2) {
+		seconds = strtol(argv[1], NULL, 10);
+		if (!seconds) {
+			printUsage();
+			return 1;
+		}
+		sleep(seconds);
+	}
+	else if (argc > 2) {
+		printUsage();
+		return 1;
+	}
+
+	return 0;
+}

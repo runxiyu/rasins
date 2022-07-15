e5c7e36bf1d3e6b84b9310383932aae2b0e70b33
Ferass 'Vitali64' EL HAFIDI <vitali64pmemail@protonmail.com>
2022-07-15 19:02:57 +0200

    core/mv: finish it!
    Signed-off-by: Ferass 'Vitali64' EL HAFIDI <vitali64pmemail@protonmail.com>

--- core/mv.c
+++ core/mv.c
@@ -18,28 +18,46 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
+#include <fcntl.h>
 
 int getopt(int argc, char *const argv[], const char *optstring);
 
 void printUsage() {
 	printf("Ferass' Base System.\n\n"
-	"Usage: mv <FILE1> <FILE2>\n\n"
-	"Move FILE1 to FILE2.\n\n");
+	"Usage: mv [-if] <FILE1> <FILE2>\n\n"
+	"Move FILE1 to FILE2.\n\n"
+	"\t-i\tIf FILE2 exists, ask for confirmation\n"
+	"\t-f\tNever ask for confirmation\n");
 }
 
 int main(int argc, char *const argv[]) {
-	int argument;
-
-	while ((argument = getopt(argc, argv, "")) != -1) {
-		if (argument == '?' || argc != 3) {
+	int argument, file;
+	char cmd, param[256];
+	setvbuf(stdout, NULL, _IONBF, 0);
+	while ((argument = getopt(argc, argv, "if")) != -1) {
+		if (argument == '?') {
 			printUsage();
 			return 1;
 		}
+		param[argument] = argument;
+		if (argument == 'f') param['i'] = 0;
+		if (argument == 'i') param['f'] = 0;
 	}
-	if (argc != 3) {
+	if (!param['f']) param['i'] = 'i';
+	if (argc < 3) {
 		printUsage();
 		return 1;
 	}
+	if ((file = open(argv[2], O_RDONLY)) != -1 && param['i']) {
+		printf("File exists, override it? (y/n) ");
+		read(STDIN_FILENO, &cmd, 3);
+		if (cmd == 'n' || cmd == 'N') {
+			close(file);
+			return 0;
+		}
+	}
+	close(file); /* In case it hasn't been closed */
+
 	rename(argv[1], argv[2]); /* Technically, moving files == renaming files */
 	if (errno) return errno;
 	return 0;

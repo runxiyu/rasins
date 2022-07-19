3341dc6d463b895dc004085e856c3808a904e895
Ferass 'Vitali64' EL HAFIDI <vitali64pmemail@protonmail.com>
2022-07-19 17:55:15 +0200

    docs/TODO.md: Update && core/ln: attempt at adding all POSIX options
    Signed-off-by: Ferass 'Vitali64' EL HAFIDI <vitali64pmemail@protonmail.com>

--- core/ln.c
+++ core/ln.c
@@ -24,15 +24,19 @@ int getopt(int argc, char *const argv[], const char *optstring);
 
 void printUsage() {
 	printf("Ferass' Base System.\n\n"
-	"Usage: ln [-s] <SOURCE> ... <TARGET>\n\n"
+	"Usage: ln [-fs] [-P|-L] <SOURCE> ... <TARGET>\n\n"
 	"Link SOURCE-s to TARGET.\n\n"
 	"\t-s\tCreate symbolic links instead of hard links\n"
-	"\t-f\tIf TARGET exists, override the file");
+	"\t-f\tIf TARGET exists, override the file\n"
+	"\t-P\tIf SOURCE names a symbolic link, create a hard link to the " 
+	"symbolic link itself (default)\n"
+	"\t-L\tIf SOURCE names a symbolic link, create a hard link to the "
+	"file referenced by the symbolic link\n");
 }
 
 int main(int argc, char *const argv[]) {
 	int argument;
-	char param[256], *params = "s";
+	char param[256], *params = "s", *buffer;
 
 	if (argc == 1) {
 		printUsage();
@@ -47,11 +51,20 @@ int main(int argc, char *const argv[]) {
 	}
 
 	for (int i = 1; i < argc - 1; i++) {
-		if (argv[argc - 1] == '-') argc--;
+		if (argv[argc - 1][0] == '-') argc--;
 		if (argv[i][0] != '-') {
 			if (param['f']) remove(argv[argc - 1]);
 			if (param['s']) symlink(argv[i], argv[argc - 1]);
+			/* The -P option is the default behavior (at least on musl), 
+			 * so no if statement. 
+			 */
+			else if (param['L']) {
+				readlink(argv[i], buffer, strlen(buffer)); /* Read the link */
+				if (errno) return errno;
+				link(buffer, argv[argc - 1]);
+			}
 			else link(argv[i], argv[argc - 1]);
+			
 			if (errno) return errno;
 		}
 	}
--- docs/TODO.md
+++ docs/TODO.md
@@ -9,3 +9,43 @@
 - Test on lots of unix/unix-like operating systems
 
 - Add lots of utils
+
+### chmod.c
+
+- support "a+rwx" and such and -R
+
+### chown.c
+
+- fix
+
+### date.c
+
+- support dates as operands
+
+### ln.c
+
+- support -L/-P
+
+- support this: `$ ln [-fs] [-L|-P] src target_dir`
+
+### ls.c
+
+- broken permissions, -l follows symlinks
+
+### mkdir.c
+
+- implement -p
+
+### more.c
+
+- implement more options
+
+### printf.c
+
+- fix string formatting
+
+### rm.c
+
+- implement all options, rm -R doesn't recursively
+remove directories
+

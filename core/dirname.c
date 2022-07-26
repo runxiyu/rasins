2781f32c80ed32a0b2579181ea6df074e763a4fd
Ferass 'Vitali64' EL HAFIDI <vitali64pmemail@protonmail.com>
2022-07-26 21:14:53 +0200

    core/: add dirname!
    Signed-off-by: Ferass 'Vitali64' EL HAFIDI <vitali64pmemail@protonmail.com>

---
+++ core/dirname.c
@@ -0,0 +1,46 @@
+/*	dirname - return directory portion of STRING
+ *	Copyright (C) 2022 Ferass EL HAFIDI
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libgen.h>
+
+int  getopt(int argc, char *const argv[], const char *optstring);
+void printUsage();
+
+int main(int argc, char *argv[]) {
+	int argument, status;
+	long unsigned int i;
+	char *dirnamestr;
+
+	if (argc != 2) {
+		printUsage();
+		return 1;
+	}
+	dirnamestr = dirname(argv[1]);
+
+	printf("%s\n", basenamestr);
+	return 0;
+}
+
+void printUsage() {
+	printf("Ferass' Base System.\n\n"
+	"Usage: dirname string\n\n"
+	"Return directory portion of <string>.\n\n");
+}

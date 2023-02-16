f97b9c7c803e7d8bc6a489083b4a30e7d5afd6aa
Ferass El Hafidi <vitali64pmemail@protonmail.com>
2023-02-16 17:43:01 +0100

    all: use SPDX identifiers and add a new MAINTAINERS file.
    Signed-off-by: Ferass El Hafidi <vitali64pmemail@protonmail.com>

--- box.c
+++ box.c
@@ -1,19 +1,4 @@
-/*	fasesiab - Ferass' Base System in a box 
- *	Copyright (C) 2022 Ferass EL HAFIDI
- *
- *	This program is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
- *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
- *
- *	You should have received a copy of the GNU General Public License
- *	along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+/* SPDX-License-Identifier: GPL-3.0-or-later */
 /* START */
 
 #include <string.h>
@@ -120,4 +105,4 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 /*  END  */
-/* Generated on Wed 11 Jan 18:03:07 CET 2023 */
+/* Generated on Thu 16 Feb 17:27:41 CET 2023 */

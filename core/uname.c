4976fb58bbf1bafc1c96121bab0d3a4cff6f9621
Ferass El Hafidi <vitali64pmemail@protonmail.com>
2023-03-31 18:13:43 +0200

    core/uname: fix argument parsing
    Signed-off-by: Ferass El Hafidi <vitali64pmemail@protonmail.com>

--- core/uname.c
+++ core/uname.c
@@ -34,12 +34,12 @@ int main(int argc, char *const argv[]) {
 
 	uname(&name);
 	if (errno) return errprint(argv0, NULL, errno);
-	if (param['s']) printf("%s ", name.sysname);
+	if (param['s'] || !(param['m'] && param['v'] && param['r'] && param['n']))
+		printf("%s ", name.sysname);
 	if (param['n']) printf("%s ", name.nodename);
 	if (param['r']) printf("%s ", name.release);
 	if (param['v']) printf("%s ", name.version);
 	if (param['m']) printf("%s", name.machine);
-	if (argc == 0)  printf("%s ", name.sysname);
 	printf("\n");
 	return errprint(argv0, NULL, errno);
 }

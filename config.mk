d702d63c2a66d9adaf9c350eeebbf79a41e524b9
Ferass El Hafidi <vitali64pmemail@protonmail.com>
2023-04-08 16:02:25 +0200

    Makefile: add '-pedantic' to CFLAGS
    Signed-off-by: Ferass El Hafidi <vitali64pmemail@protonmail.com>

--- config.mk
+++ config.mk
@@ -5,7 +5,7 @@ VERSION=r"$$(git rev-list --count HEAD)"."$$(git rev-parse --short HEAD)"
 CC=cc # C Compiler
 FORCEC99=-std=c99 # Force POSIX C99
 # OPTIMIZE=-O3 # Experimental optimizations (Disabled by default)
-CFLAGS=-D_POSIX_C_SOURCE=200809L -DVERSION=\"$(VERSION)\" -I. -Wall -Werror -Wextra -g $(FORCEC99) $(OPTIMIZE) # C Compiler flags
+CFLAGS=-D_POSIX_C_SOURCE=200809L -DVERSION=\"$(VERSION)\" -I. -Wall -Werror -Wextra -g -pedantic $(FORCEC99) $(OPTIMIZE) # C Compiler flags
 NOLINKER=-c # C Compiler flag to disable the linker
 # Core utilities
 CORE=\

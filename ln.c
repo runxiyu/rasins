c8ff1b667050cc51732b8b04d2789cab463e0c88
Runxi Yu <me@runxiyu.org>
2024-06-09 08:00:00 +0800

    ln: Count arguments after parsing options
    Previously, argc is always expected to be 3 (program name, source,
    destination), without regard for any arguments. This check was done
    before parsing options, so effectively, the usage of any options would
    cause the check to fail.
    
    This commit causes argument counting to happen after options parsing.
    
    This commit also notes how -L is completely broken due to its attempt to
    use an unallocated buffer. This should be fixed later.

--- ln.c
+++ ln.c
@@ -20,10 +20,6 @@ int main(int argc, char *const argv[])
 	int argument;
 	char param[256], *buffer = NULL, *argv0 = strdup(argv[0]);
 
-	if (argc != 3) {
-		print_usage(argv[0], DESCRIPTION, OPERANDS, VERSION);
-		return 1;
-	}
 	while ((argument = getopt(argc, argv, "fsPL")) != -1) {
 		if (argument == '?') {
 			print_usage(argv[0], DESCRIPTION, OPERANDS, VERSION);
@@ -33,6 +29,10 @@ int main(int argc, char *const argv[])
 	}
 	argc -= optind;
 	argv += optind;
+	if (argc != 2) {
+		print_usage(argv0, DESCRIPTION, OPERANDS, VERSION);
+		return 1;
+	}
 	if (param['f'])
 		remove(argv[1]);
 	if (errno && errno != ENOENT)
@@ -40,15 +40,25 @@ int main(int argc, char *const argv[])
 	errno = 0;		/* Not reached if errno == ENOENT (no such file) */
 	if (param['s'])
 		symlink(argv[0], argv[1]);
-	/* The -P option is the default behavior (at least on musl), 
-	 * so no if statement. 
-	 */
 	else if (param['L']) {
-		readlink(argv[0], buffer, strlen(buffer));	/* Read the link */
+		/* -L intentionally ignored if -s */
+		/* FIXME: Buffer is unallocated, so it's completely broken
+		 * Note that readlink(2) (on Linux at least) truncates the
+		 * contents if the buffer is not long enough. See the NOTES
+		 * section in its man page.
+		 */
+		readlink(argv[0], buffer, strlen(buffer));
 		if (errno)
 			return errprint(argv0, argv[0], errno);
 		link(buffer, argv[1]);
 	} else
+		/* The -P option is the default behavior (at least on musl),
+		 * so no if statement.
+		 */
+		/*
+		 * FIXME: What if the user wants to override a prior -s with -P?
+		 * The way fases handles options is unordered, so... idk.
+		 */
 		link(argv[0], argv[1]);
-	return errprint(argv0, argv[1], errno);
+	return errprint(argv0, argv[0], errno);	// FIXME: This assumes that errno applies to argv[0]
 }
